1e2e47262e59c3e8aed2a422981da9246fd4be79
颜子楠 <yanzinan@bj-zkhd.com>
2017-12-29 18:05:13 +0800

    Add Python

---
+++ 002_Python基础及应用/Exp/Exp1/Exp1.py
@@ -0,0 +1,99 @@
+# 交互模式
+# 命令行模式
+# 可以用来执行.py文件
+
+
+# Helloworld
+##print("Helloworld")
+#print("fffffff")
+
+
+# 变量与算术表达式
+##Var_int = 11111111111111111111111111111
+##print(Var_int)
+
+
+# 输入输出
+##name  = input("Please input Your name.")
+##print("Hellow",name)
+##print("Hellow"+name)
+##//
+##/*   */
+
+
+# 数据类型
+# 整形、任意大小
+##a = 10
+
+# 浮点、1.24 x 10^3  = 1.24e3
+##a = 10.0
+
+# 字符串 使用双引号或单引号括起来，如果需要一起使用，可以使用转意符'\'
+##a = "10"
+# 多行字符串，可以使用不转意符"r"
+a = """sssss
+ssssssssss
+sssssss"""
+a= r""" ddddddddddddd """
+##a[0]可以访问字符串
+
+
+# 布尔量
+# True
+# False
+
+# 运算符
+##print("1+1 =",1+1)
+##print("2x3 =",2*3)
+##print("6/2 =",6/2)
+##print("2/6 =",2/6)
+##print("2//6 =",2//6) # 地板除 因为类似 floor()
+##print("2^5 =",2**5)
+##i = 0
+##i++
+##i+=1
+##其中一个位浮点数时，计算结果也为浮点数
+
+# 列表
+# 申请一个列表
+##a = []
+##b = ["fff",123,True,10.3]
+##b.append() #在后面 添加一个节点
+##b.clear() #清除列表
+##b.remove(x) #删除某个节点
+##b.extend(x) #拼接一个x列表
+
+# 元组
+#a = (2)
+
+# 条件语句
+##a = 1
+##b = 2
+##if (a>b) :
+##    print("a>b")
+##else:
+##    print("a<b")
+    
+##a = 1;
+##b = 2;
+##if(a>b)
+##{
+##    print(xxxxx)
+##    }else
+##{
+##    print(TTTTT)
+##    }
+#No switch
+
+# 循环语句
+for i in range(5):
+    if i == 3:
+        continue
+    print("这是第",i,"次循环")
+print("End")
+##for i in XXXX:
+##    YYYYYYYY
+# 这是一个遍历函数，用来遍历所有range里的数据
+##类似于c++里的foreach()
+#break
+#continue
